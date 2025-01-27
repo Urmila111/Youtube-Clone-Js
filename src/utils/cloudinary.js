8dc3b314853858f9da7216421a4ad6c19730f07c
Urmila111 <basneturmi777@gmail.com>
2025-01-27 11:41:34 +0545

    bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,11 +3,11 @@ import fs from "fs"
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_CLOUD_SECRET
 });
 
-const uploadCloludinary = async (localFilePath)=> {
+const uploadOnCloudinary = async (localFilePath)=> {
     try {
         if(!localFilePath) return null
         //upload the file on cloudinary
