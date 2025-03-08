21cf2aba16ce4cf9c92f6ea833f8fc70dd1e213e
Urmila111 <basneturmi777@gmail.com>
2025-03-08 08:21:01 +0545

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
-    username:{
+    userName:{
        type: String, 
        required: true,
        unique: true,
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next){
    if(!this.isModified("password"))  return next();
-   this.password = bcrypt.hash(this.password, 10)
+   this.password = await bcrypt.hash(this.password, 10)
    next()
 })
 
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function(){
          _id: this._id,
          email: this.email,
          username: this.username,
-         fullname: this.fullname
+         fullName: this.fullName
       }, 
       process.env.ACCESS_TOKEN_SECRET,
       {
